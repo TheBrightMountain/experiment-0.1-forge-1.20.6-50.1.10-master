dcbb4c43adc3741a91bec35b6e6ec27b39973345
ADMIN <minhson6a1@gmail.com>
2024-07-16 16:42:14 +0700

    Comment

---
+++ src/main/java/net/redofdoom/experiment/item/ModCreativeModeTabs.java
@@ -0,0 +1,30 @@
+package net.redofdoom.experiment.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.redofdoom.experiment.Experiment;
+import net.redofdoom.experiment.block.ModBlocks;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Experiment.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> EXPERIMENTAL_TAB = CREATIVE_MODE_TAB.register("experimental_tab", () -> CreativeModeTab.builder()
+            .icon(() -> new ItemStack(ModItems.RUBY.get()))
+            .title(Component.translatable("creativetab.experimental_tab"))
+            .displayItems((itemDisplayParameters, output) ->
+            {
+            output.accept(ModItems.RUBY.get());
+            output.accept(ModBlocks.RUBY_BLOCK.get());
+            })
+            .build());
+
+    public static void register(IEventBus eventBus)
+    {
+           CREATIVE_MODE_TAB.register(eventBus);
+    }
+}
